54a34fe44acb7f82d6f785d2150a5cb7a44e04b4
folzieds <folzieds@gmail.com>
2022-03-15 09:31:50 +0100

    Added index template to project

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>test</h1>"
+    return render_template('index.html')
 
 @app.route('/api/v1/health')
 def health():
