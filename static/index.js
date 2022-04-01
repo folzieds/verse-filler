5bb4481744d2533223104db2598afe8ec2d22762
folzieds <folzieds@gmail.com>
2022-04-01 23:36:26 +0100

    Added javascript for clearing form after submitting

--- static/index.js
+++ static/index.js
@@ -1,16 +1,5 @@
-// const form = document.getElementById('my_form');
-
-
-// form.onsubmit = async (event) => { 
-//   event.preventDefault();
-
-//   let response = await fetch('/api/v1/fill',{
-//     method:'POST',
-//     body: new FormData(form)
-//   });
-
-//   let result = await response.data;
-
-
-//   form.reset();
-// };
+document.querySelector('form').onsubmit = e => {
+  e.target.submit();
+  e.target.reset();
+  return false;
+};
