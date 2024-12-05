938f38261d3e23d59595897772ed9abe8e335064
Osu Adefolarin <folzieds@gmail.com>
2024-12-05 14:57:45 +0100

    fixed issue with regex

--- verseFiller.py
+++ verseFiller.py
@@ -15,44 +15,43 @@ def fill_verse_inplace(filename: str):
     doc = docx.Document(filename)
 
     for para in doc.paragraphs:
-        if(is_verse(para.text)):
+        if is_verse(para.text):
             para.text = fill_verse(para.text)
             para.runs[0].bold = True
 
     doc.save(filename)
 
 def is_verse(text: str) -> bool:
-    if(re.search('[1-3]*\s[A-Za-z]*\s[0-9]*:\s[1-9]*\s-\s[0-9]*', text) != None):
+    if re.search(r'[1-3]*\s[A-Za-z]*\s[0-9]*:\s[1-9]*\s-\s[0-9]*', text) is not None:
         return True
-    elif (re.search('[A-Za-z]*\s[0-9]*:\s[0-9]*\s-\s[0-9]*', text)) != None:
+    elif (re.search(r'[A-Za-z]*\s[0-9]*:\s[0-9]*\s-\s[0-9]*', text)) is not None:
         return True
-    elif(re.search('[1-3]*\s[A-Za-z]*\s[0-9]*:\s[0-9]*', text) != None):
+    elif re.search(r'[1-3]*\s[A-Za-z]*\s[0-9]*:\s[0-9]*', text) is not None:
         return True
-    elif (re.search('[A-Za-z]*\s[0-9]*:\s[0-9]*', text)) != None:
+    elif (re.search(r'[A-Za-z]*\s[0-9]*:\s[0-9]*', text)) is not None:
         return True
     else:
         return False
 
 def fill_verse(text: str) -> str:
-    if(re.search('[1-3]*\s[A-Za-z]*\s[0-9]*:\s[1-9]*\s-\s[0-9]*', text) != None):
-        return add_range_verse_text(re.search('[1-3]*\s[A-Za-z]*\s[0-9]*:\s[0-9]*\s-\s[0-9]*', text).group())
-    elif (re.search('[A-Za-z]*\s[0-9]*:\s[0-9]*\s-\s[0-9]*', text)) != None:
-        return add_range_verse_text(re.search('[A-Za-z]*\s[0-9]*:\s[0-9]*\s-\s[0-9]*', text).group())
-    elif(re.search('[1-3]*\s[A-Za-z]*\s[0-9]*:\s[0-9]*', text) != None):
-        return add_verse_text(re.search('[1-3]*\s[A-Za-z]*\s[0-9]*:\s[0-9]*', text).group())
-    elif (re.search('[A-Za-z]*\s[0-9]*:\s[0-9]*', text)) != None:
-        return add_verse_text(re.search('[A-Za-z]*\s[0-9]*:\s[0-9]*', text).group())
+    if re.search(r'[1-3]*\s[A-Za-z]*\s[0-9]*:\s[1-9]*\s-\s[0-9]*', text) is not None:
+        return add_range_verse_text(re.search(r'[1-3]*\s[A-Za-z]*\s[0-9]*:\s[0-9]*\s-\s[0-9]*', text).group())
+    elif (re.search(r'[A-Za-z]*\s[0-9]*:\s[0-9]*\s-\s[0-9]*', text)) is not None:
+        return add_range_verse_text(re.search(r'[A-Za-z]*\s[0-9]*:\s[0-9]*\s-\s[0-9]*', text).group())
+    elif re.search(r'[1-3]*\s[A-Za-z]*\s[0-9]*:\s[0-9]*', text) is not None:
+        return add_verse_text(re.search(r'[1-3]*\s[A-Za-z]*\s[0-9]*:\s[0-9]*', text).group())
+    elif (re.search(r'[A-Za-z]*\s[0-9]*:\s[0-9]*', text)) is not None:
+        return add_verse_text(re.search(r'[A-Za-z]*\s[0-9]*:\s[0-9]*', text).group())
     else:
         pass
 
 def add_verse_text(verse: str) -> str:
     # split the verse into book chapter and verse
-    book = ""
-    if(re.search("[0-9]*\s[a-zA-z]*",verse).group().strip() != ''):
-        book = re.search("[0-9]*\s[a-zA-z]*",verse).group().strip()
+    if(re.search(r"[0-9]*\s[a-zA-z]*",verse).group().strip() != ''):
+        book = re.search(r"[0-9]*\s[a-zA-z]*",verse).group().strip()
     else:
-        book = re.search("[a-zA-z]*",verse).group().strip()
-    chapter = int(re.split("[a-zA-z]",verse.split(":",maxsplit = 2)[0])[-1].strip())
+        book = re.search(r"[a-zA-z]*",verse).group().strip()
+    chapter = int(re.split(r"[a-zA-z]",verse.split(":",maxsplit = 2)[0])[-1].strip())
     ver = int(verse.split(":",maxsplit = 2)[-1].strip())
     # get the text from the dataframe
     try:
@@ -66,12 +65,11 @@ def add_verse_text(verse: str) -> str:
 
 def add_range_verse_text(verse: str) -> str:
     # split the verse into book chapter and verse
-    book = ""
-    if(re.search("[0-9]*\s[a-zA-z]*",verse).group().strip() != ''):
-        book = re.search("[0-9]*\s[a-zA-z]*",verse).group().strip()
+    if(re.search(r"[0-9]*\s[a-zA-z]*",verse).group().strip() != ''):
+        book = re.search(r"[0-9]*\s[a-zA-z]*",verse).group().strip()
     else:
-        book = re.search("[a-zA-z]*",verse).group().strip()
-    chapter = int(re.split("[a-zA-z]",verse.split(":",maxsplit = 2)[0])[-1].strip())
+        book = re.search(r"[a-zA-z]*",verse).group().strip()
+    chapter = int(re.split(r"[a-zA-z]",verse.split(":",maxsplit = 2)[0])[-1].strip())
     ver_start = int(verse.split(":",maxsplit = 2)[1].strip().split("-")[0].strip())
     ver_end = int(verse.split(":",maxsplit = 2)[1].strip().split("-")[1].strip())
 
